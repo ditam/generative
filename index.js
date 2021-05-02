ef4e913b4af686cf09682ec6b68479e31a58e6d6
ditam <ditam@users.noreply.github.com>
2021-05-02 21:10:45 +0200

    fix jquery reference in index

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-  console.log('Hello!');
-
   $('.buttons .button').on('click', function() {
     const newStyle = $(this).data('style');
     $('.buttons .button').removeClass('selected');
